21db359e87494c5217b6bf4866c7f70686b907eb
Khuselo Nditha <khuselonditha@gmail.com>
2025-02-17 14:03:19 +0200

    After creating the run function and testing that we correctly connect to our website.

--- property24_browser.py
+++ property24_browser.py
@@ -2,9 +2,21 @@ import asyncio
 from playwright.async_api import async_playwright
 
 
+async def run(pw):
+    site = "https://www.property24.com/"
+
+    print("Connecting to scraping browser...")
+    browser = await pw.chromium.launch()
+    page = await browser.new_page()
+    await page.goto(site)
+    print(await page.title())
+    await browser.close()
+
+
 async def main():
-    async with async_playwright as playwright:
+    async with async_playwright() as playwright:
         await run(playwright)
+
 
 
 if __name__ == "__main__":
