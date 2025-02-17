f1caf1c7df67562253dfd4d1cca5978eb7143d1f
Khuselo Nditha <khuselonditha@gmail.com>
2025-02-17 13:49:15 +0200

    After adding the main function

--- property24_browser.py
+++ property24_browser.py
@@ -2,7 +2,9 @@ import asyncio
 from playwright.async_api import async_playwright
 
 
-
+async def main():
+    async with async_playwright as playwright:
+        await run(playwright)
 
 
 if __name__ == "__main__":
