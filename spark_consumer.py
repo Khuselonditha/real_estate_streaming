2f101e4294d783dd4a0bd17066bee3c297a4ebdf
Khuselo Nditha <khuselonditha@gmail.com>
2025-02-27 00:51:10 +0200

    After making the schema lowercase

--- spark_consumer.py
+++ spark_consumer.py
@@ -9,7 +9,7 @@ from pyspark.sql.functions import from_json, col
 def create_keyspace(session):
     session.execute("""
         CREATE IF NOT EXISTS property_streams
-        WITH replication = {'class': 'SimpleStrategy', replication_factor: '1'};
+        WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'};
     """)
 
     logging.info("Keyspace created successfully.")
@@ -25,7 +25,7 @@ def create_table(session):
             bedrooms TEXT,
             bathrooms TEXT,
             parking TEXT,
-            link TEXT,
+            link TEXT PRIMARY KEY,
             type_of_property TEXT,
             lifestyle TEXT,
             listing_date TEXT,
@@ -33,8 +33,7 @@ def create_table(session):
             no_transfer_duty TEXT,
             rates_and_taxes TEXT,
             pets_allowed TEXT,
-            pictures list<TEXT>,
-            PRIMARY KEY (link)
+            pictures list<TEXT>
         );          
     """)
 
@@ -51,7 +50,7 @@ def insert_data(session, **kwargs):
             pictures)
         VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
     """
-    
+
     try:
         session.execute(query, **kwargs.values())
         logging.info("Data inserted successfully.")
@@ -67,6 +66,8 @@ def create_cassandra_session():
         if session is not None:
             create_keyspace(session)
             create_table(session)
+        
+        return session
 
     except Exception as e:
         logging.error(f"Couldn't create the cassandra connection due to {e}")
@@ -88,26 +89,26 @@ def main():
     kafka_df = (spark_conn.readStream.format("kafka")
             .option("kafka.bootstrap.servers", "localhost:9092")
             .option("subscribe", "properties")
-            .option("startingOffests", "earliest")
+            .option("startingOffsets", "earliest")
             .load())
     
     schema = StructType([
-        StructField("Title", StringType(), True),
-        StructField("Address", StringType(), True),
-        StructField("Price", StringType(), True),
-        StructField("Size", StringType(), True),
-        StructField("Bedrooms", StringType(), True),
-        StructField("Bathrooms", StringType(), True),
-        StructField("Parking", StringType(), True),
-        StructField("Link", StringType(), True),
-        StructField("Type of Property", StringType(), True),
-        StructField("Lifestyle", StringType(), True),
-        StructField("Listing Date", StringType(), True),
+        StructField("title", StringType(), True),
+        StructField("address", StringType(), True),
+        StructField("price", StringType(), True),
+        StructField("size", StringType(), True),
+        StructField("bedrooms", StringType(), True),
+        StructField("bathrooms", StringType(), True),
+        StructField("parking", StringType(), True),
+        StructField("link", StringType(), True),
+        StructField("type_of_property", StringType(), True),
+        StructField("lifestyle", StringType(), True),
+        StructField("listing_date", StringType(), True),
         StructField("levies", StringType(), True),
-        StructField("No Trasfer Duty", StringType(), True),
-        StructField("Rates and Taxes", StringType(), True),
-        StructField("Pets Allowed", StringType(), True),
-        StructField("Pictures", ArrayType(StringType()), True)
+        StructField("no_trasfer_duty", StringType(), True),
+        StructField("rates_and_taxes", StringType(), True),
+        StructField("pets_allowed", StringType(), True),
+        StructField("pictures", ArrayType(StringType()), True)
     ])
 
     kafka_df = (kafka_df.selectExpr("CAST(value AS STRING) as value")
@@ -115,8 +116,10 @@ def main():
            .select("data.*"))
     
     cassandra_query = (kafka_df.writeStream
-                       .foreachBatch(lambda batch_df, batch_id: batch_df.foreach(
+                        .foreachBatch(lambda batch_df, batch_id: batch_df.foreach(
                            lambda row: insert_data(create_cassandra_session(), **row.asDict())))
+                        .start()
+                        .awaitTermination()
                        )
 
 
