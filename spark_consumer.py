633d36b3c9e382451037aa590f74681a2ca51ea3
Khuselo Nditha <khuselonditha@gmail.com>
2025-02-26 14:54:59 +0200

    After adding schema

--- spark_consumer.py
+++ spark_consumer.py
@@ -2,6 +2,8 @@ import logging
 
 from cassandra.cluster import Cluster
 from pyspark.sql import SparkSession
+from pyspark.sql.types import StringType, StructField, StructType, ArrayType
+
 
 
 def main():
@@ -20,6 +22,25 @@ def main():
             .option("subscribe", "properties")
             .option("startingOffests", "earliest")
             .load())
+    
+    schema = StructType([
+        StructField("title", StringType(), True),
+        StructField("address", StringType(), True),
+        StructField("price", StringType(), True),
+        StructField("size", StringType(), True),
+        StructField("bedrooms", StringType(), True),
+        StructField("bathrooms", StringType(), True),
+        StructField("parking", StringType(), True),
+        StructField("link", StringType(), True),
+        StructField("type_of_property", StringType(), True),
+        StructField("lifestyle", StringType(), True),
+        StructField("listing_date", StringType(), True),
+        StructField("levies", StringType(), True),
+        StructField("trasfer_dute", StringType(), True),
+        StructField("rates_and_taxes", StringType(), True),
+        StructField("pets", StringType(), True),
+        StructField("pictures", ArrayType(StringType()), True)
+    ])
 
 if __name__ == "__main__":
     main()
