e65cb256258e316cb1ca8c1909d99f31d65544f3
Khuselo Nditha <khuselonditha@gmail.com>
2025-02-26 13:41:59 +0200

    After creating the main function and connecting to kafka and cassandra

--- spark_consumer.py
+++ spark_consumer.py
@@ -4,5 +4,16 @@ from cassandra.cluster import Cluster
 from pyspark.sql import SparkSession
 
 
+def main():
+    # Logging config
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+
+    spark_conn = (SparkSession.builder
+            .appName('RealEstateConsumer') 
+            .config('spark.cassandra.connection.host', 'localhost')
+            .config('spark.jars.packages', "com.datastax.spark:spark-cassandra-connector_2.12:3.5.0,"
+                                           "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0")
+            .getOrCreate())
+
 if __name__ == "__main__":
     main()
