aef3ff4cba4b650028db24187e1ebc7578566685
Khuselo Nditha <khuselonditha@gmail.com>
2025-02-26 13:50:41 +0200

    After creating kafka_df

--- spark_consumer.py
+++ spark_consumer.py
@@ -14,6 +14,12 @@ def main():
             .config('spark.jars.packages', "com.datastax.spark:spark-cassandra-connector_2.12:3.5.0,"
                                            "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0")
             .getOrCreate())
+    
+    kafka_df = (spark_conn.readStream.format("kafka")
+            .option("kafka.bootstrap.servers", "localhost:9092")
+            .option("subscribe", "properties")
+            .option("startingOffests", "earliest")
+            .load())
 
 if __name__ == "__main__":
     main()
